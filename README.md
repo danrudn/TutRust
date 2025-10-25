3829c9c4c7236c662a674eb958b997a009aa2955
DanRudn <daniel_herrmann22@web.de>
2025-10-25 12:01:16 +0200

    add basics datatypes

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # TutRust
+
+## Basics
+### Installation Linux
+1. download: `curl https://sh.rustup.rs -sSf | sh` 
+2. Rust PATH in zshrc oder bashrc einfügen: `. "$HOME/.cargo/env" `
+
+### Rust package contains
+1. rustc: compiler
+2. cargo: build/package tool 
+
+### project and cargo 
+1. create new project: `cargo new my_project` name should be snake case
+2. build and run: `cargo run my_project` 
+
+### VsCode Extension
+1. rust-analyzer
+2. linkedProjects for rust-analyzer
+```
+.vscode -> settings.json (folder settings)
+{
+  "rust-analyzer.linkedProjects": [
+    ".pathto/Cargo.toml"
+  ]
+}
+```
+
---
+++ basics/datatypes/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+
+    // mutable and immutable
+    {
+        let mut mut_integer: i32 = 12;
+        mut_integer = 50; // mutable -> OK
+        let imut_integer: i32 = 12; 
+        //immutInt = 50; // immutable -> not ok
+    }
+
+    // primitive typs
+    {
+        let integer: i32 = 42;
+        let float: f64 = 3.14;
+        let boolean: bool = true;
+        println!("primitives: {integer} {float} {boolean}");
+    }
+    // tuple array
+    {
+        let tuple: (i32, f64) = (10, 2.5);
+        let array: [i32; 3] = [1, 2, 3]; // array [type, length]
+        println!("Tuple: ({}, {})", tuple.0, tuple.1);
+        println!("Array: {:?}", array);
+    }
+
+    // string
+    {
+        let str_slice: &str = "Hallo";
+        let mut string: String = String::from("Rust");
+        string.push('!');
+        println!("Strings: {str_slice} {string}");
+    }
+    
+    // shdowing
+    {
+        let x = 5;          
+        let x = x * 2;      // new variable shadows  variable x from before
+        println!("Variablen: x={x}");
+    }
+
+    // casting
+    let num = 65;
+    let num_f64: f64 = num as f64;
+    println!("Casting: num:{num} -> num as f64: {num_f64}");
+}
